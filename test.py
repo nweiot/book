1d674df5f1c9f6fdcbe0b5aacaf9bacc24b72a76
jongwookkim <whddnr2558@naver.com>
2022-05-24 00:55:22 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,28 @@
+
+def login(id_user, pw_user):
+    choice = input("1.회원가입\n2.로그인\n3.나가기")
+    if choice == '1':
+        id_user = input("등록할 아이디를 입력해주세요.")
+        pw_user = input("등록할 비밀번호를 입력해주세요.")
+        print("회원가입이 완료되었습니다.")
+        login(id_user, pw_user)
+    elif choice == '2':
+        id = input("아이디를 입력해주세요.")
+        pw = input("비밀번호를 입력해주세요.")
+        if id_user == id and pw_user == pw:
+            pass
+        else:
+            print("ID 또는 PW를 확인해주세요.")
+            login(id_user, pw_user)
+    elif choice == '3':
+        exit(1)
+    else:
+        login(id_user, pw_user)
+
+def search():
+    choice=input("검색 방식 설정\n1.번호 조회\n2.도서 이름 조회\n3.저자 이름 조회\n4.종료")
+
+
+
+
+login(None, None)
